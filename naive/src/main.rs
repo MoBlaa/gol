b926e3d06718365844b5dfc0444658df919f57a9
Moritz Hepp <moritz.hepp@gmx.de>
2021-08-17 13:05:48 +0200

    added time measurement

--- naive/src/main.rs
+++ naive/src/main.rs
@@ -1,5 +1,5 @@
 use naive::{Field, Strategy};
-use tokio::time::Duration;
+use tokio::time::{Duration, Instant};
 
 #[tokio::main]
 async fn main() {
@@ -10,16 +10,18 @@ async fn main() {
 
     let mut round = 1usize;
     loop {
-        tokio::time::sleep(Duration::from_millis(500)).await;
+        tokio::time::sleep(Duration::from_millis(250)).await;
 
+        let now = Instant::now();
         let field = strategy.next();
+        let elapsed = now.elapsed();
         if field.is_none() {
-            println!("Extinction is inevitable...");
+            println!("Reached stable state after {} rounds.", round + 1);
             break;
         }
         let field = field.unwrap();
 
-        println!("Round {}:\n{}", round, field);
+        println!("Round {} ({:?}):\n{}", round, elapsed, field);
         round = round + 1;
     }
 }
