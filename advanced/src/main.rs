65da346aba23d2b75e8ff2538b86f21327a43060
Moritz Hepp <moritz.hepp@gmx.de>
2021-08-17 15:43:45 +0200

    created new module with advanced implementation

---
+++ advanced/src/main.rs
@@ -0,0 +1,62 @@
+use gol_adv::Strategy;
+use gol_lib::Field;
+use std::collections::hash_map::DefaultHasher;
+use std::collections::HashSet;
+use std::hash::{Hash, Hasher};
+use tokio::time::{Duration, Instant};
+
+fn hash(field: &Field) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    field.hash(&mut hasher);
+    hasher.finish()
+}
+
+#[tokio::main]
+async fn main() {
+    let mut args = std::env::args();
+    args.next();
+    let width: usize = args.next().map(|s| s.parse().unwrap_or(100)).unwrap_or(100);
+
+    let height: usize = args.next().map(|s| s.parse().unwrap_or(100)).unwrap_or(100);
+    let timeout: u64 = args.next().map(|s| s.parse().unwrap_or(250)).unwrap_or(250);
+    let print: bool = args
+        .next()
+        .map(|s| s.parse().unwrap_or(false))
+        .unwrap_or(false);
+
+    let field = Field::random(width, height);
+    if print {
+        println!("Round 0:\n{}", field);
+    }
+
+    let mut strategy = Strategy::new(field.clone());
+
+    let mut round = 1usize;
+    let mut visited = HashSet::new();
+    visited.insert(hash(&field));
+    let mut whole = Duration::new(0, 0);
+    loop {
+        tokio::time::sleep(Duration::from_millis(timeout)).await;
+
+        let now = Instant::now();
+        let field = strategy.next();
+        let elapsed = now.elapsed();
+
+        whole += elapsed;
+
+        if field.is_none() {
+            break;
+        }
+        let field = field.unwrap();
+        if !visited.insert(hash(&field)) {
+            break;
+        }
+
+        if print {
+            println!("Round {} ({:?})", round, elapsed);
+            println!("{}", field);
+        }
+        round += 1;
+    }
+    println!("Finished after {}rnd and {:?}", round + 1, whole);
+}
