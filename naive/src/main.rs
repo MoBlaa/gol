7e1aa96f791ba5baad65fddb18d89280d95cc45f
Moritz Hepp <moritz.hepp@gmx.de>
2021-08-17 11:48:28 +0200

    added iteration and abortion if nothing changes anymore

--- naive/src/main.rs
+++ naive/src/main.rs
@@ -1,6 +1,11 @@
 use std::fmt;
 use std::fmt::Write;
+use tokio::time::Duration;
 
+const ALIVE: char = '▣';
+const DEAD: char = '▢';
+
+#[derive(Clone)]
 struct Field<const WIDTH: usize, const HEIGHT: usize> {
     inner: [[char; WIDTH]; HEIGHT],
 }
@@ -11,7 +16,7 @@ impl<const WIDTH: usize, const HEIGHT: usize> Field<WIDTH, HEIGHT> {
 
         for x in 0..WIDTH {
             for y in 0..HEIGHT {
-                *field.value_mut((x, y)) = if rand::random::<bool>() { '▣' } else { '▢' }
+                *field.value_mut((x, y)) = if rand::random::<bool>() { ALIVE } else { DEAD }
             }
         }
 
@@ -23,7 +28,7 @@ impl<const WIDTH: usize, const HEIGHT: usize> Field<WIDTH, HEIGHT> {
             panic!("minimum size of a field is 3x3");
         }
         Self {
-            inner: [[' '; WIDTH]; HEIGHT],
+            inner: [[DEAD; WIDTH]; HEIGHT],
         }
     }
 
@@ -78,23 +83,84 @@ impl<const WIDTH: usize, const HEIGHT: usize> fmt::Display for Field<WIDTH, HEIG
     }
 }
 
+struct Strategy<const WIDTH: usize, const HEIGHT: usize> {
+    field: Field<WIDTH, HEIGHT>,
+}
+
+impl<const WIDTH: usize, const HEIGHT: usize> Strategy<WIDTH, HEIGHT> {
+    fn new(field: Field<WIDTH, HEIGHT>) -> Self {
+        Strategy { field }
+    }
+
+    /// Returns the resulting value of one cell if it changes.
+    fn advance_one(&self, cords: (usize, usize)) -> Option<char> {
+        let neighbours = self.field.neighbours(cords);
+        let value = self.field.value(cords);
+
+        let alive = neighbours.iter().filter(|char| char == &&ALIVE).count();
+
+        // Breakdown of the rules
+        /*
+        1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+        2. Any live cell with two or three live neighbours lives on to the next generation.
+        3. Any live cell with more than three live neighbours dies, as if by overpopulation.
+        4.Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+         */
+        match (value, alive == 2, alive == 3) {
+            (&ALIVE, true, _) => None,
+            (&ALIVE, _, true) => None,
+            (&DEAD, _, true) => Some(ALIVE),
+            (&ALIVE, _, _) => Some(DEAD),
+            _ => None,
+        }
+    }
+}
+
+impl<const WIDTH: usize, const HEIGHT: usize> Iterator for Strategy<WIDTH, HEIGHT> {
+    type Item = Field<WIDTH, HEIGHT>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut field = Field::new();
+
+        let mut updated_any = false;
+        for x in 0..WIDTH {
+            for y in 0..HEIGHT {
+                if let Some(value) = self.advance_one((x, y)) {
+                    *field.value_mut((x, y)) = value;
+                    updated_any = true;
+                }
+            }
+        }
+
+        if !updated_any {
+            return None;
+        }
+
+        self.field = field.clone();
+
+        Some(field)
+    }
+}
+
 #[tokio::main]
 async fn main() {
-    let field = Field::<20, 10>::random();
-    println!("Hello, Field:\n{}", field);
+    let field = Field::<100, 10>::random();
+    println!("Round 0:\n{}", field);
 
-    let print_neighbours = |cords: (usize, usize)| {
-        let neighbours = field.neighbours(cords);
-        println!(
-            "Neighbours of '{}' {:?} are: {:?}",
-            field.value(cords),
-            cords,
-            neighbours
-        );
-    };
+    let mut strategy = Strategy::new(field);
 
-    print_neighbours((0, 0));
-    print_neighbours((19, 0));
-    print_neighbours((0, 9));
-    print_neighbours((19, 9));
+    let mut round = 1usize;
+    loop {
+        tokio::time::sleep(Duration::from_millis(500)).await;
+
+        let field = strategy.next();
+        if field.is_none() {
+            println!("Extinction is inevitable...");
+            break;
+        }
+        let field = field.unwrap();
+
+        println!("Round {}:\n{}", round, field);
+        round = round + 1;
+    }
 }
