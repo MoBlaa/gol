347082bbf66f535755ee1b97b5cc33fa825dca5f
Mo <mo.blaa@pm.me>
2021-08-17 08:19:46 +0200

    Added field.struct

--- naive/src/main.rs
+++ naive/src/main.rs
@@ -1,4 +1,27 @@
+use std::fmt;
+
+struct Field<const WIDTH: usize, const HEIGHT: usize> {
+    inner: [[char; WIDTH]; HEIGHT]
+}
+
+impl<const WIDTH: usize, const HEIGHT: usize> Field<WIDTH, HEIGHT> {
+    fn new() -> Self {
+        Self { inner: [[' '; WIDTH]; HEIGHT] }
+    }
+}
+
+impl<const WIDTH: usize, const HEIGHT: usize> fmt::Display for Field<WIDTH, HEIGHT> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for row in self.inner {
+            writeln!(f, "{}", row.iter().collect::<String>())?;
+        }
+        Ok(())
+    }
+}
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let field = Field::<5, 6>::new();
+    println!("Hello, Field:\n{}", field);
 }
+
