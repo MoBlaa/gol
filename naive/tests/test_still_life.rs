a42db6893d2946a64a33328d75de90578692c69b
Moritz Hepp <moritz.hepp@gmx.de>
2021-08-17 12:09:21 +0200

    dried

--- naive/tests/test_still_life.rs
+++ naive/tests/test_still_life.rs
@@ -1,15 +1,7 @@
 use naive::{Field, Strategy, ALIVE, DEAD};
 
-#[test]
-fn test_block() {
-    let field = Field::from([
-        [DEAD, DEAD, DEAD, DEAD],
-        [DEAD, ALIVE, ALIVE, DEAD],
-        [DEAD, ALIVE, ALIVE, DEAD],
-        [DEAD, DEAD, DEAD, DEAD],
-    ]);
-
-    let mut strategy = Strategy::new(field.clone());
+fn is_still<const WIDTH: usize, const HEIGHT: usize>(field: Field<WIDTH, HEIGHT>) {
+    let mut strategy = Strategy::new(field);
 
     let next = strategy.next();
     assert!(
@@ -20,82 +12,56 @@ fn test_block() {
 }
 
 #[test]
+fn test_block() {
+    is_still(Field::from([
+        [DEAD, DEAD, DEAD, DEAD],
+        [DEAD, ALIVE, ALIVE, DEAD],
+        [DEAD, ALIVE, ALIVE, DEAD],
+        [DEAD, DEAD, DEAD, DEAD],
+    ]));
+}
+
+#[test]
 fn test_beehive() {
-    let field = Field::from([
+    is_still(Field::from([
         [DEAD, DEAD, DEAD, DEAD, DEAD, DEAD],
         [DEAD, DEAD, ALIVE, ALIVE, DEAD, DEAD],
         [DEAD, ALIVE, DEAD, DEAD, ALIVE, DEAD],
         [DEAD, DEAD, ALIVE, ALIVE, DEAD, DEAD],
         [DEAD, DEAD, DEAD, DEAD, DEAD, DEAD],
-    ]);
-
-    let mut strategy = Strategy::new(field.clone());
-
-    let next = strategy.next();
-    assert!(
-        next.is_none(),
-        "Advanced on still life to:\n{}",
-        next.unwrap()
-    );
+    ]));
 }
 
 #[test]
 fn test_loaf() {
-    let field = Field::from([
+    is_still(Field::from([
         [DEAD, DEAD, DEAD, DEAD, DEAD, DEAD],
         [DEAD, DEAD, ALIVE, ALIVE, DEAD, DEAD],
         [DEAD, ALIVE, DEAD, DEAD, ALIVE, DEAD],
         [DEAD, DEAD, ALIVE, DEAD, ALIVE, DEAD],
         [DEAD, DEAD, DEAD, ALIVE, DEAD, DEAD],
         [DEAD, DEAD, DEAD, DEAD, DEAD, DEAD],
-    ]);
-
-    let mut strategy = Strategy::new(field.clone());
-
-    let next = strategy.next();
-    assert!(
-        next.is_none(),
-        "Advanced on still life to:\n{}",
-        next.unwrap()
-    );
+    ]));
 }
 
 #[test]
 fn test_boat() {
-    let field = Field::from([
+    is_still(Field::from([
         [DEAD, DEAD, DEAD, DEAD, DEAD],
         [DEAD, ALIVE, ALIVE, DEAD, DEAD],
         [DEAD, ALIVE, DEAD, ALIVE, DEAD],
         [DEAD, DEAD, ALIVE, DEAD, DEAD],
         [DEAD, DEAD, DEAD, DEAD, DEAD],
-    ]);
-
-    let mut strategy = Strategy::new(field.clone());
-
-    let next = strategy.next();
-    assert!(
-        next.is_none(),
-        "Advanced on still life to:\n{}",
-        next.unwrap()
-    );
+    ]));
 }
 
 #[test]
 fn test_tub() {
-    let field = Field::from([
+    is_still(Field::from([
         [DEAD, DEAD, DEAD, DEAD, DEAD],
         [DEAD, DEAD, ALIVE, DEAD, DEAD],
         [DEAD, ALIVE, DEAD, ALIVE, DEAD],
         [DEAD, DEAD, ALIVE, DEAD, DEAD],
         [DEAD, DEAD, DEAD, DEAD, DEAD],
-    ]);
-
-    let mut strategy = Strategy::new(field.clone());
-
-    let next = strategy.next();
-    assert!(
-        next.is_none(),
-        "Advanced on still life to:\n{}",
-        next.unwrap()
-    );
+    ]));
 }
