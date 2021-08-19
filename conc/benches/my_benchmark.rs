e1123c1532a1879f9a8a6756ddfcb3ad89f4ad59
Moritz Hepp <moritz.hepp@gmx.de>
2021-08-19 11:15:38 +0200

    made field bigger and updated names

--- conc/benches/my_benchmark.rs
+++ conc/benches/my_benchmark.rs
@@ -3,8 +3,8 @@ use gol_conc::Strategy;
 use gol_lib::Field;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("gol 1000 1000", |b| {
-        let field = Field::random(1000, 1000);
+    c.bench_function("gol-conc 10000 10000", |b| {
+        let field = Field::random(10000, 10000);
 
         let mut strategy = Strategy::new(field);
         b.iter(|| strategy.next());
