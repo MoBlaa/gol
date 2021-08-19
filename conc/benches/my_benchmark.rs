ee71e89f0beec017a328c84aab612c10840a9b73
Moritz Hepp <moritz.hepp@gmx.de>
2021-08-19 11:21:33 +0200

    fixed gol-conc bench name

--- conc/benches/my_benchmark.rs
+++ conc/benches/my_benchmark.rs
@@ -3,7 +3,7 @@ use gol_conc::Strategy;
 use gol_lib::Field;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("gol-conc 1000 10000", |b| {
+    c.bench_function("gol-conc 1000 1000", |b| {
         let field = Field::random(1000, 1000);
 
         let mut strategy = Strategy::new(field);
