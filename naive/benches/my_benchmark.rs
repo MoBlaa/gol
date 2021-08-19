92cc3cb8ace3b2ddad23507c2271d7e32b4afb63
Moritz Hepp <moritz.hepp@gmx.de>
2021-08-19 11:27:47 +0200

    added some benchmarks

--- naive/benches/my_benchmark.rs
+++ naive/benches/my_benchmark.rs
@@ -9,6 +9,24 @@ fn criterion_benchmark(c: &mut Criterion) {
         let mut strategy = Strategy::new(field);
         b.iter(|| strategy.next());
     });
+    c.bench_function("gol-naive 100 100", |b| {
+        let field = Field::random(100, 100);
+
+        let mut strategy = Strategy::new(field);
+        b.iter(|| strategy.next());
+    });
+    c.bench_function("gol-naive 1000 100", |b| {
+        let field = Field::random(1000, 100);
+
+        let mut strategy = Strategy::new(field);
+        b.iter(|| strategy.next());
+    });
+    c.bench_function("gol-naive 100 1000", |b| {
+        let field = Field::random(100, 1000);
+
+        let mut strategy = Strategy::new(field);
+        b.iter(|| strategy.next());
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
